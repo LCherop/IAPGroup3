ba39a3c25d0677111f1c861df65be412c3b789e3
Laura Cherop <77297998+LCherop@users.noreply.github.com>
2021-03-05 15:14:20 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Form</title>
+</head>
+<body>
+	<div class="main">
+		<header>
+		<p>Message Sender</p>
+		</header>
+	    <div class="mainBox" style="position: absolute;">
+		    <form method="post" action="integreter.php" >
+                <p>
+                
+                <input type="text" name="fname" placeholder="Full Name">
+                </p>
+
+                <p>
+                <input type="text" name="pnumber" placeholder="Phone Number">
+                </p>
+
+                <p>
+                <input type="text" name="message" placeholder="Message">
+                </p>
+
+                <p>
+                <p><input type="submit" name="send"></p>
+                </p>
+		    </form>
+	    </div>
+	</div>
+</body>
+</html>
